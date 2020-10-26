74358101fd0e8e07ce2fd410566afdedc328b1c5
Johanna Maria Prins <joprin@ttu.ee>
2020-10-26 11:53:52 +0200

    project structure and classes

---
+++ ApplicationCore/Entities/Basket/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities.Basket
+{
+    class ShoppingCart
+    {
+
+        private int ShoppingCartID;
+
+    }
+}
---
+++ ApplicationCore/Entities/Customer/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities.Customer
+{
+    public class Account
+    {
+        public String accountID;
+        public DateTime dateOpened;
+        public DateTime dateClosed;
+
+    }
+}
