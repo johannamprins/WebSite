eb25a9506e3dd47a1ef2e10f3c23fa4e9a2a6059
Loora Kaljaste <lokalj@ttu.ee>
2020-10-29 12:58:11 +0200

    added classes

--- ApplicationCore/Entities/Order/Account.cs
+++ ApplicationCore/Entities/Order/Account.cs
@@ -6,5 +6,6 @@ namespace ApplicationCore.Entities.Order
 {
     class Account
     {
+
     }
 }
--- ApplicationCore/Entities/Order/Payment.cs
+++ ApplicationCore/Entities/Order/Payment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace ApplicationCore.Entities.Order
 {
     class Payment
