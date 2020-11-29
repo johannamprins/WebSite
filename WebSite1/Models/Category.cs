8cb42967664c9c61eeb389337961b9d9dacf949d
Hanna Kaljaste <hakalj@ttu.ee>
2020-11-29 14:18:14 +0200

    Adding Entity Framework and DbContext

--- WebSite1/Models/Category.cs
+++ WebSite1/Models/Category.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace WebSite1.Models
 {
@@ -10,7 +11,9 @@ namespace WebSite1.Models
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
-        public List<Item> Items { get; set; } 
+        public List<Item> Items { get; set; }
+
+
 
         //models store all the data we need to work with our application
         // in order to process the data, we will need repositories - dependency injection
