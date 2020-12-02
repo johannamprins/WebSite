f29fb6bcb4e06a72dc5d82574808e1212fb4b8af
Loora Kaljaste <lokalj@ttu.ee>
2020-12-02 13:33:49 +0200

    Adding navigation bar

---
+++ WebSite1/Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite1.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
---
+++ WebSite1/Controllers/IndexController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite1.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
