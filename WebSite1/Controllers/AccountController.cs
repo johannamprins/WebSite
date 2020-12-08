c8994bbac0b533ee0c8a00d0845e860bdefc5a2a
mariaTimofejev <33320925+mariaTimofejev@users.noreply.github.com>
2020-12-08 18:11:28 +0200

    Adding AccountController

---
+++ WebSite1/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Account _signInManager;
+        private Account _userManager;
+
+        public AccountController(Account signInManager, Account userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        // GET: /Account/Login
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        // POST: /Account/Login
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
