25d2b2271dc596e66f8526a6235d5b2d2764c88f
Loora Kaljaste <lokalj@ttu.ee>
2020-12-08 12:53:53 +0200

    Adding shopping card

---
+++ WebSite1/Models/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebSite1.Models
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _appDbContext;
+        public string ShoppingCardId { get; set; }
+        public List<ShoppingCartItem> shoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>
+                ()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+            string cardId = session.GetString("CardId") ?? Guid.NewGuid().ToString();
+            session.SetString("CardId", cardId);
+
+            return new ShoppingCart(context) { ShoppingCardId = cardId};
+        }
+
+    }
+}
