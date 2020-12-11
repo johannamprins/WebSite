d9879892e6fdf58bac4c5bb6dce800cf424d5226
mariaTimofejev <33320925+mariaTimofejev@users.noreply.github.com>
2020-12-11 14:16:41 +0200

    Add To Cart Button

--- WebSite1/Models/Classes/Account.cs
+++ WebSite1/Models/Classes/Account.cs
@@ -1,7 +1,7 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using WebSite1.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSite1.Controllers;
 
 //namespace WebSite1
 //{
