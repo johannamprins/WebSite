66e41f41cd1ed76d92c414499f74497f95263a70
Loora Kaljaste <lokalj@ttu.ee>
2020-12-12 16:54:31 +0200

    Creating custom email tag helper

---
+++ WebSite1/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebSite1.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string LinkText { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailuto:" + Address);
+            output.Content.SetContent(LinkText);
+        }
+    }
+}
